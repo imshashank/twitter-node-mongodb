13e8ecea848deb73dc1fc2c545b65c041fc41b66
shashank agarwal <shashank@shankee.com>
2013-10-12 16:35:11 -0400

    Update credentials.js
    Just add your twitter api keys. get them from dev.twitter.com

--- credentials.js
+++ credentials.js
@@ -1,8 +1,8 @@
 var credentials = {
-    consumer_key: 'NaLI20aNEh9DO8HKjAONgQ',
-    consumer_secret: 'jipDuqOD4mUrzDFPei4F7vj0Wv76xXkLM8ImZQ7Sw',
-    access_token_key: '16464939-Ey0bS4Rf2xHQ9zIjCEF8fgFMi0HOX2ik9hQ85cBwx',
-    access_token_secret: 'VkzVpzC8mko3DP9OTwh35HoZpdedMY6icdXZennfw'
+    consumer_key: 'your',
+    consumer_secret: 'twitter',
+    access_token_key: 'API',
+    access_token_secret: 'KEYS'
 };
 
 module.exports = credentials;
